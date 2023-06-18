25451f989078e6820d5947939a4c96935bd0fcbd
daniel.aitken <daniel.aitken@thinkingbig.net>
2023-06-17 22:09:50 -0300

    adding some logging

--- database/connect.py
+++ database/connect.py
@@ -2,9 +2,13 @@
 Connection management functionality and base utilities for the database.
 '''
 
+import logging
 from sqlalchemy import create_engine, URL, Engine
 from sqlalchemy.orm import Session
 from utils import config
+
+
+logger = logging.getLogger(f'civviebot.{__name__}')
 
 
 def get_db() -> Engine:
@@ -15,6 +19,7 @@ def get_db() -> Engine:
         f'{config.CIVVIEBOT_DB_DIALECT}+{config.CIVVIEBOT_DB_DRIVER}',
         **config.DB_URL_KWARGS
     )
+    logger.info(f'Connecting to civviebot database at {url.__to_string__()}')
     database = create_engine(url)
     return database
 
